561e419a387a70521d87893d9302ca5c59f52441
abokizh <abokizh@mail.ru>
2024-04-12 21:38:20 -0400

    Part 1

--- solution_q1.py
+++ solution_q1.py
@@ -1,16 +1,18 @@
 import numpy as np
 from collections import defaultdict
+import matplotlib.pyplot as plt
+from matplotlib.patches import Patch
 import gymnasium as gym
-env = gym.make('Blackjack-v1', natural=False, sab=False)
+env = gym.make('Blackjack-v1', natural=False, sab=False, render_mode="human")
 ###YOUR Q-LEARNING CODE BEGINS
 
 ## Properties
-episodes = 500
+episodes = 1000000
 rate = 0.001
 discount = 0.87
 exploration_value = 1.0
 exploration_terminal = 0.1
-exploration_discount = exploration_value / (episodes / 3) 
+exploration_discount = exploration_value/(episodes/3) 
 
 ## RL Agent
 class Agent:
@@ -50,14 +52,36 @@ for episode in range(episodes):
 
     game_over = False
     observation, info = env.reset()
+
     while not game_over:
         action = agent.move(observation)
         new_observation, reward, terminated, truncated, info = env.step(action)
         agent.update(observation, action, reward, terminated, new_observation)
+        # total_reward += reward
         observation = new_observation
         game_over = terminated or truncated
 
-    # total_reward += reward
     agent.decrease_exploration()
-# print("Total reward: ", reward)
+# print("Total reward: ", total_reward)
+# Plot the results
+rolling_length = 500
+fig, axs = plt.subplots(ncols=2, figsize=(12, 5))
+axs[0].set_title("Episode rewards")
+reward_moving_average = (
+    np.convolve(
+        np.array(env.return_queue).flatten(), np.ones(rolling_length), mode="valid"
+    )
+    / rolling_length
+)
+axs[0].plot(range(len(reward_moving_average)), reward_moving_average)
+axs[1].set_title("Episode lengths")
+length_moving_average = (
+    np.convolve(
+        np.array(env.length_queue).flatten(), np.ones(rolling_length), mode="same"
+    )
+    / rolling_length
+)
+axs[1].plot(range(len(length_moving_average)), length_moving_average)
+plt.tight_layout()
+plt.show()
 ###YOUR Q-LEARNING CODE ENDS
